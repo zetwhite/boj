4281cae0b0d9e5d0d14f48b9a2a6f69430776b09
zetwhite <zetwhite@naver.com>
2021-01-27 09:56:02 +0900

    add programmers _ greedy6 solution

--- programmers/greedy6.cpp
+++ programmers/greedy6.cpp
@@ -1,60 +1,33 @@
 #include <string>
 #include <vector>
 #include <algorithm> 
-#include <unordered_map>
-#include <set> 
-#include <iostream> 
+#include <queue>
 
 using namespace std;
 
-bool cmp(set<int>& a, set<int>& b){
-  return a.size() > b.size(); 
-}
-
-int findMax(unordered_map<int, set<int>>& connect){
-  int maxInt = 0; 
-  int maxSz = 0; 
-  for(auto it : connect){
-    if(it.second.size() > maxSz){
-      maxSz = it.second.size(); 
-      maxInt = it.first; 
-    }
-  }
-  return maxInt; 
+bool cmp(vector<int>& a, vector<int>& b){
+  return a[1] > b[1]; 
 }
 
 int solution(vector<vector<int>> routes) {
-    int answer = 0;
-    int sz = routes.size() * 2; 
-    unordered_map<int, set<int>> connect; 
-    for(int i = 0; i < routes.size() * 2; i++){
-      int q = i/2; 
-      int l = i%2; 
-      for(int k = 0; k < routes.size(); k++){
-        if(routes[k][0] <= routes[q][l] && routes[q][l] <= routes[k][1])
-          connect[i].insert(k); 
-      }
-    } 
-    //sort(connect, connect + sz, cmp); 
-    while(!connect.empty()){
-      int idx = findMax(connect); 
-      set<int> del = connect[idx]; 
-      for(auto i : del){
-        connect[i].clear(); 
-      }
-      for(int i = 0; i < sz; i++){
-        if(connect.find(i) == connect.end())
-          continue; 
-        for(auto j : del){
-          cout << "at = " << i << ", del = " << j << endl; 
-          auto f = connect[i].find(j); 
-          if(f != connect[i].end())
-           connect[i].erase(f); 
-        }
-      }
+  int answer = 0; 
+  sort(routes.begin(), routes.end(), cmp); 
+  priority_queue<int, vector<int>,  less<int>> pq;
+  int idx = 1; 
+  pq.push(routes[0][0]); 
+  for(; idx < routes.size(); idx++){
+    int newLineStart = routes[idx][1]; 
+    int newLineEnd = routes[idx][0]; 
+    if( pq.top() > newLineStart ){
       answer++; 
+      pq = priority_queue<int, vector<int>, less<int>>(); 
     }
-    return answer;
+    pq.push(routes[idx][0]); 
+  }
+  if(!pq.empty()){
+    answer++; 
+  }
+  return answer; 
 }
 
 int main(){
